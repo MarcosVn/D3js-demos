a2bf9faaa6ecbd71408a22e3d2403d67e20d91ed
MarcosVn <marcos.vnaraujo@gmail.com>
2020-04-07 22:38:50 -0300

    first commit

---
+++ assets/charts/js/timeseriesplot.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    let data = [
+        { 'value': 1380854103662 },
+        { 'value': 1363641921283 }
+    ];
+    
+    timeseries('timeseries', data, true);
+});
